0f395acf0a9ad46f65bb5b578c86279b121d4417
TiffThuan <117796555+TiffThuan@users.noreply.github.com>
2024-09-06 15:53:00 -0500

    Update get.php

--- get.php
+++ get.php
@@ -4,7 +4,7 @@ $pageTitle= " Get";
 include "view-header.php";
 ?>
     <h1>Get</h1>
-<form method ="post" action="get-result.php">
+<form method ="get" action="get-result.php">
   <input type="text" name="my-name">
   <input type="submit" value="Get data">
 </form> 
