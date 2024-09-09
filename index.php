961ae18121693f91c7d217ea439118d950289de5
TiffThuan <117796555+TiffThuan@users.noreply.github.com>
2024-09-09 14:37:16 -0500

    Update index.php

--- index.php
+++ index.php
@@ -1,9 +1,12 @@
-<?php
-$pageTitle= " Home";
 
+<?php
+$pageTitle = "Home";
 include "view-header.php";
 ?>
-    <h1>Home!</h1>
+<h1>Welcome to Homework 2!</h1>
+<p>This site demonstrates the use of PHP for handling form data using GET and POST methods.</p>
+<p>Utilize the navigation bar to explore different forms and see how wonderful data is processed.</p>
 <?php
-include"view-footer.php";
+include "view-footer.php";
 ?>
+
