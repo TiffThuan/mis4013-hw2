38ab45ae4a2ae1327c41e4defb0e2cac0eaf1de0
TiffThuan <117796555+TiffThuan@users.noreply.github.com>
2024-09-09 18:01:34 -0500

    Update post.php

--- post.php
+++ post.php
@@ -1,17 +1,3 @@
-// <?php
-// $pageTitle= " Post";
-
-// include "view-header.php";
-// ?>
-//     <h1>Post</h1>
-// <form method ="post" action="post-result.php">
-//   <input type="text" name="my-name">
-//   <input type="submit" value="Post data">
-// </form> 
-// <?php
-// include"view-footer.php";
-// ?>
-
 
 <?php
 $pageTitle = "Post";
