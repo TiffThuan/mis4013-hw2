f75a7ed7056a0b5a7d8439ba29cf0630caa6edcc
TiffThuan <117796555+TiffThuan@users.noreply.github.com>
2024-09-09 14:38:33 -0500

    Update post.php

--- post.php
+++ post.php
@@ -1,13 +1,31 @@
-<?php
-$pageTitle= " Post";
+// <?php
+// $pageTitle= " Post";
 
+// include "view-header.php";
+// ?>
+//     <h1>Post</h1>
+// <form method ="post" action="post-result.php">
+//   <input type="text" name="my-name">
+//   <input type="submit" value="Post data">
+// </form> 
+// <?php
+// include"view-footer.php";
+// ?>
+
+
+<?php
+$pageTitle = "Post";
 include "view-header.php";
 ?>
-    <h1>Post</h1>
-<form method ="post" action="post-result.php">
-  <input type="text" name="my-name">
-  <input type="submit" value="Post data">
-</form> 
+<h1>POST Method Form</h1>
+<p>Use the form below to send data using the POST method. The data will be displayed on the results page.</p>
+<form method="post" action="post-result.php">
+  <div class="mb-3">
+    <label for="my-name" class="form-label">Enter your name:</label>
+    <input type="text" id="my-name" name="my-name" class="form-control">
+  </div>
+  <button type="submit" class="btn btn-primary">Post data</button>
+</form>
 <?php
-include"view-footer.php";
+include "view-footer.php";
 ?>
