795b30338a67eedd28832ef8bca59003b9d92f04
TiffThuan <117796555+TiffThuan@users.noreply.github.com>
2024-09-09 18:02:44 -0500

    Update get.php

--- get.php
+++ get.php
@@ -1,17 +1,3 @@
-// <?php
-// $pageTitle= " Get";
-
-// include "view-header.php";
-// ?>
-//     <h1>Get</h1>
-// <form method ="get" action="get-result.php">
-//   <input type="text" name="my-name">
-//   <input type="submit" value="Get data">
-// </form> 
-// <?php
-// include"view-footer.php";
-// ?>
-
 
 <?php
 $pageTitle = "Get";
