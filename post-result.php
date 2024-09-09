bc2118b0287478802ffca7cf8ba6464c2d5f91f4
TiffThuan <117796555+TiffThuan@users.noreply.github.com>
2024-09-09 18:11:12 -0500

    Update post-result.php

--- post-result.php
+++ post-result.php
@@ -1,28 +1,18 @@
-// <?php
-// $pageTitle= " Post-result";
 
-// include "view-header.php";
-// ?>
-//     <h1>Post Result</h1>
-// <?php
-// echo getDisplay;
-// include"view-footer.php";
-
-// function getDisplay() {
-//     if (isset($_POST['my-name'])) {
-//         return " <p> The value sent is: </p>". $_POST['my-name'];
-// } else {
-//   return "<p> Nothing posted to the page </p>";
-// }
-// }
-// ?>
 
 <?php
 $pageTitle = "Post Result";
 include "view-header.php";
 ?>
+
 <h1>POST Result</h1>
+
 <?php
+echo getDisplay();
+?>
+<?php
+include "view-footer.php";
+
 function getDisplay() {
   if (isset($_POST['my-name'])) {
     return "<p class='alert alert-success'>The value sent is: " . htmlspecialchars($_POST['my-name']) . "</p>";
@@ -30,8 +20,4 @@ function getDisplay() {
     return "<p class='alert alert-warning'>Nothing posted to the page</p>";
   }
 }
-echo getDisplay();
-?>
-<?php
-include "view-footer.php";
 ?>
