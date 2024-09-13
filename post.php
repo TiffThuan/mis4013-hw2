5a65cd45e2c78a3c249a4b44e0c3a13f9114b23c
TiffThuan <117796555+TiffThuan@users.noreply.github.com>
2024-09-13 16:24:33 -0500

    Update post.php

--- post.php
+++ post.php
@@ -1,19 +1,24 @@
 
-
-
 <?php
-$pageTitle= "Contact Us";
+$pageTitle = "Contact Us";
 
 include "view-header.php";
 ?>
     <h1>Contact Us</h1>
-<form method ="post" action="post-result.php">
-  <label for="customer-name">Enter your name:</label>
-  <input type="text" name="customer-name" id="customer-name">
-  <label for="message">Enter your message:</label>
-  <textarea name="message" id="message"></textarea>
-  <input type="submit" value="Send">
-</form> 
+    <form method="post" action="post-result.php" class="mb-3">
+        <div class="mb-3">
+            <label for="customer-name" class="form-label">Enter your name:</label>
+            <input type="text" class="form-control" name="customer-name" id="customer-name" required>
+        </div>
+        <div class="mb-3">
+            <label for="message" class="form-label">Enter your message:</label>
+            <textarea class="form-control" name="message" id="message" rows="4" required></textarea>
+        </div>
+        <div class="mb-3">
+            <input type="submit" class="btn btn-primary" value="Send">
+        </div>
+    </form> 
 <?php
 include "view-footer.php";
 ?>
+
