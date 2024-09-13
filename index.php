3293aaac4fd2d53a7304c04e6c2fb5aa4659985b
TiffThuan <117796555+TiffThuan@users.noreply.github.com>
2024-09-12 23:52:19 -0500

    Update index.php

--- index.php
+++ index.php
@@ -1,3 +1,4 @@
+
 <?php
 $pageTitle = "Welcome to Our King Coffee Shop";
 
@@ -5,6 +6,39 @@ include "view-header.php";
 ?>
     <h1>Welcome to Our King Coffee Shop!</h1>
     <p>Experience the best Vietnamese coffee - Cafe Sua Da. Explore our menu and place your orders online!</p>
+
+    <h2>Our Coffee Menu</h2>
+    <table class="table table-striped">
+        <thead>
+            <tr>
+                <th scope="col">Coffee</th>
+                <th scope="col">Price</th>
+            </tr>
+        </thead>
+        <tbody>
+            <tr>
+                <td>Espresso</td>
+                <td>$3.00</td>
+            </tr>
+            <tr>
+                <td>Americano</td>
+                <td>$3.50</td>
+            </tr>
+            <tr>
+                <td>Cappuccino</td>
+                <td>$4.00</td>
+            </tr>
+            <tr>
+                <td>Latte</td>
+                <td>$4.50</td>
+            </tr>
+            <tr>
+                <td>Cafe Sua Da</td>
+                <td>$5.00</td>
+            </tr>
+        </tbody>
+    </table>
+
 <?php
 include "view-footer.php";
 ?>
